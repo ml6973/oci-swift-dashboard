30ace575583594ea4da8c18fcb09d303de9b4008
gregoryhooks <gregoryhooksjr@gmail.com>
2017-08-14 12:14:29 -0500

    Bootstrap now hosted locally

--- WebContent/views/MasterView.class.php
+++ WebContent/views/MasterView.class.php
@@ -16,10 +16,6 @@ class MasterView {
 			<link href="css/bootstrap.min.css" rel="stylesheet">
 			<link href="css/custom.css" rel="stylesheet">
 
-			<!--
-			<link rel="stylesheet" href="https://bootswatch.com/cerulean/bootstrap.css" media="screen">
-		    <link rel="stylesheet" href="https://bootswatch.com/assets/css/custom.min.css">
-		    -->
 			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
 				<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 			<link href="http://getbootstrap.com/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
@@ -37,7 +33,7 @@ class MasterView {
 				<!--Jquery-->
 			<script src="http://code.jquery.com/jquery-latest.js"></script>
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-			<script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+			<script src="js/bootstrap/bootstrap.min.js"></script>
 		    	<!--bootstrap-->
 		    		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 			<script src="http://getbootstrap.com/assets/js/ie10-viewport-bug-workaround.js"></script>
